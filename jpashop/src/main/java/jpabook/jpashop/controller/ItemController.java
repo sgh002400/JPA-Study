21f05589e3c377ec8a83e192602d692af2ba1244
jihwan <sgh002400@naver.com>
2021-08-01 23:58:46 +0900

    상품 정보 수정에 변경 감지와 병합(merge) 개념 적용하기

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -67,14 +67,23 @@ public class ItemController {
         return "items/updateItemForm";
     }
 
+    //궁금증 -> 그럼 여기가 준영속 상태인 것은 db에서 findOne으로 객체를 넣어준게 아니라 단지 new를 통해 form의 값들을 넣어줘서 그런거?
+    //db에 한번 저장된건 무조건 준영속이 아니라?
+
     /** 상품 수정 **/
     @PostMapping(value = "/items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form) { //@ModelAttribute -> 뷰 페이지에서 form이라는 이름으로 값을 가져올 수 있게 해준다.
+    public String updateItem(@PathVariable long itemId, @ModelAttribute("form") BookForm form) { //@ModelAttribute -> 뷰 페이지에서 form이라는 이름으로 값을 가져올 수 있게 해준다.
 
         //이 유저가 이 아이템에 대해서 수정할 권한이 있는지 체크해줘야 악의적으로 수정하는 것을 막을 수 있다.
 
+        /**
         Book book = new Book();
-        book.setId(form.getId());
+        book.setId(form.getId()); //id가 있다는건 JPA를 통해 DB에 저장이 되었다는 뜻! -> 이런걸 준영속 엔티티라고 한다!
+        //JPA가 관리하는 영속 상태 엔티티는 변경 감지가 일어난다. 하지만 준영속 엔티티는 영속성 컨텍스트의 지원을 받을 수 없고 데이터를 수정해도 변경 감지 기능은 동작X
+        //준영속 엔티티를 수정하는 2가지 방법
+        //변경 감지 기능 사용 -> itemService의 updateItem() 참고
+        //병합( merge ) 사용
+
         book.setName(form.getName());
         book.setPrice(form.getPrice());
         book.setStockQuantity(form.getStockQuantity());
@@ -82,6 +91,10 @@ public class ItemController {
         book.setIsbn(form.getIsbn());
 
         itemService.saveItem(book);
+        **/
+
+        //좋은 방법!
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
 
         return "redirect:/items";
     }
--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -18,6 +18,12 @@ public class ItemRepository {
             em.persist(item);
         } else { //DB에 등록되어 있던걸 update 한다고 일단 생각하기.
             em.merge(item);
+            //merge란? -> itemService의 updateItem()와 동일
+            //DB에서 item을 찾는다. 그리고 파라미터로 넘어온 값으로 찾아온 item의 값을 모두 바꿔버린다.
+            //그래서 commit이 일어나면 알아서 JPA가 update 쿼리 날려줌.
+            //중요!! -> em.merge(item) 여기서 item은 준영속 상태이고 만약 Item merge = em.merge(item) 했을 때 merge가 영속 상태이다. 두 개는 다른거임!!
+            //주의!! -> merge를 사용하면 모든 속성이 변경된다. 병합시 값이 없으면 null 로 업데이트 할 위험도 있다. (merge는 모든 필드를 교체한다.)
+            //그렇기 때문에 변경 감지 기능을 사용하는게 좋다!
         }
     }
 
