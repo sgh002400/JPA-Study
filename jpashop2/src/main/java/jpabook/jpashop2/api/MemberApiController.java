50a2d691fb15bfbc615d96d583dbce6dafab5b8d
jihwan <sgh002400@naver.com>
2021-08-09 00:37:08 +0900

    MemberApiController에 DTO 사용

--- jpashop2/src/main/java/jpabook/jpashop2/api/MemberApiController.java
+++ jpashop2/src/main/java/jpabook/jpashop2/api/MemberApiController.java
@@ -32,6 +32,17 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
     @Data
     static class CreateMemberRequest {
         private String name;
