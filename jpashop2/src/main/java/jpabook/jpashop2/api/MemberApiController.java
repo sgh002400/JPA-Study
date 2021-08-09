1bb251903c12393b43b9d55f7c4fd50ce35a41e2
jihwan <sgh002400@naver.com>
2021-08-09 14:17:32 +0900

    회원 수정, 조회 API 개발 - DTO Return 하기

--- jpashop2/src/main/java/jpabook/jpashop2/api/MemberApiController.java
+++ jpashop2/src/main/java/jpabook/jpashop2/api/MemberApiController.java
@@ -2,13 +2,14 @@ package jpabook.jpashop2.api;
 
 import jpabook.jpashop2.domain.Member;
 import jpabook.jpashop2.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController //@Controller + @ResponseBody
 @RequiredArgsConstructor
@@ -26,6 +27,37 @@ public class MemberApiController {
      * 결론
      * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
      */
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    /**
+     * 조회 V2: 응답 값으로 엔티티가 아닌 별도의 DTO를 반환한다.
+     */
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        //엔티티 -> DTO 변환
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    class MemberDto {
+        private String name;
+    }
+
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { //요청 바디에 담긴게 member에 담긴다
         Long id = memberService.join(member);
@@ -57,4 +89,27 @@ public class MemberApiController {
             this.id = id;
         }
     }
+
+    /**
+     * 수정 API
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName()); //업데이트는 업데이트의 기능만 수행하고 끝나야 됨!
+        Member findMember = memberService.findOne(id); //위의 이유로 조회는 따로 함
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
 }
--- jpashop2/src/main/java/jpabook/jpashop2/domain/Member.java
+++ jpashop2/src/main/java/jpabook/jpashop2/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop2.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +23,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    //@JsonIgnore //이 어노테이션을 붙인 컬럼 정보는 빼고 Member 엔티티 정보를 모두 출력해준다.
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
     //컬렉션은 필드에서 바로 초기화 하는 것이 좋다! -> null 문제에서 안전하다. / orders 컬렉션은 절대 변경하지 말것! -> 하이버네이트가 원하는대로 동작하지 않을 수 있다.
